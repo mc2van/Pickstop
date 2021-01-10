2eb9f884acc8eb7e9328e19e5a0758b9d059f935
Evan Qi <evanqi@gmail.com>
2021-01-10 17:42:31 -0500

    questions

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,15 @@
-export default Home {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <h2>Food Picker<br/>for<br/>Uber Eats</h2>
-            <p>
-              Need help choosing something to eat?
+export default function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Food Picker<br />for<br />Uber Eats</h2>
+        <p>
+          Need help choosing something to eat?
             </p>
-            <form action="https://www.ubereats.com">
-              <input type="submit" value="Get Started" />
-            </form>
-          </header>
-        </div>
-    );
+        <form action="https://www.ubereats.com">
+          <input type="submit" value="Get Started" />
+        </form>
+      </header>
+    </div>
+  )
 }
---
+++ src/q5.js
@@ -0,0 +1,5 @@
+export default function question1() {
+    return (
+        <h1>Question 5</h1>
+    )
+}
