1d6c3200e16df973c8903b9d4c578d7c40483c06
Leon Dong <leon.d1043@gmail.com>
2021-01-10 17:26:57 -0500

    moved html to home.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Food Picker<br/>for<br/>Uber Eats</h2>
-        <p>
-          Need help choosing something to eat?
-        </p>
-        <form action="https://www.ubereats.com">
-          <input type="submit" value="Get Started" />
-        </form>
-      </header>
-    </div>
-  );
+  
 }
 
 export default App;
