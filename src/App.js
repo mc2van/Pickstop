c5ba73888c560abd4bd4aa220331f93028665c84
Evan Qi <evanqi@gmail.com>
2021-01-10 19:22:27 -0500

    imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-
 import Home from "./pages/Home.js";
+import Q1 from "./pages/q1.js"
+import Q2 from "./pages/q2.js"
+import Q3 from "./pages/q3.js"
+import Q4 from "./pages/q4.js"
+import Q5 from "./pages/q5.js"
+
+function counter(i) {
+    if (i === 0) {
+        return (
+            <Home />
+        )
+    } else if (i === 1) {
+        return (
+            <Q1 />
+        )
+    } else if (i === 2) {
+        return (
+            <Q2 />
+        )
+    } else if (i === 3) {
+        return (
+            <Q3 />
+        )
+    } else if (i === 4) {
+        return (
+            <Q4 />
+        )
+    } else if (i === 4) {
+        return (
+            <Q5 />
+        )
+    }
+}
 
 function App() {
-  return(
-    <Home />
-  )
+    return (
+        <Home />
+    )
 }
 
 export default App;
