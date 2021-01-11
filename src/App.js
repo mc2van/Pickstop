675ed39cf1ea0bd73cee5c83b2902c498d616e4c
Leon Dong <leon.d1043@gmail.com>
2021-01-10 19:26:09 -0500

    result page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Q2 from "./pages/q2.js"
 import Q3 from "./pages/q3.js"
 import Q4 from "./pages/q4.js"
 import Q5 from "./pages/q5.js"
+import Result from "./pages/Result.js"
 
 function counter(i) {
     if (i === 0) {
@@ -33,6 +34,10 @@ function counter(i) {
         return (
             <Q5 />
         )
+    } else {
+      return (
+        <Result />
+      )
     }
 }
 
---
+++ src/pages/Result.js
@@ -0,0 +1,5 @@
+export default function question1() {
+    return (
+        <h1>Result</h1>
+    )
+}
