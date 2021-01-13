28c74d865c47e92df6e9556f0c99470f00f98937
Leon Dong <leon.d1043@gmail.com>
2021-01-13 18:29:18 -0500

    small changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ export default function Home({ changePage }) {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Food Picker<br />for<br />Uber Eats</h2>
+        <h2>Pickstop</h2>
         <p>
           Need help choosing something to eat?
         </p>
--- src/pages/Input.js
+++ src/pages/Input.js
@@ -3,6 +3,7 @@ export default function Input({ changePage }) {
         <div className="App">
             <header className="App-header">
                 <h1>Enter your restaurants</h1>
+                <p>(You don't have to fill all of these in)</p>
                 <button onClick={() => changePage(2)}>Submit</button>
             </header>
         </div>
