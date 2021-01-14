faf59382cbfda82b5a98770016d67a43d5da7e0f
Leon Dong <leon.d1043@gmail.com>
2021-01-13 22:37:52 -0500

    added forms and stuff

--- src/pages/Input.js
+++ src/pages/Input.js
@@ -1,10 +1,51 @@
+import { useState } from "react";
+
 export default function Input({ changePage }) {
+    const [array, setArray] = useState([]);
+    const [input, setInput] = useState("");
+
+    function populateArray() {
+        setArray([...array, input]);
+        setInput("");
+    }
+
+    function removeFromArray(index) {
+        let tempArray = array;
+    
+        tempArray.splice(index, 1);
+        setArray([...tempArray]);
+    }
+    
+      const submitInput = (e) => {
+        if (e.key === "Enter") {
+          populateArray();
+        }
+    };
+
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Enter your restaurants</h1>
-                <p>(You don't have to fill all of these in)</p>
-                <button onClick={() => changePage(2)}>Submit</button>
+                <ul>
+                    {array.map((element, i) => {
+                    return (
+                    <li key={i}>
+                        {element}
+                        <button onClick={() => removeFromArray(i)}>Remove</button>
+                    </li>
+                    );
+                    })}
+                </ul>
+                <input
+                    type="text"
+                    placeholder="Restaurant"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    onKeyDown={submitInput}
+                />
+                <div class="button-div">
+                    <button onClick={() => changePage(2)}>Submit</button>
+                </div>
             </header>
         </div>
     )
